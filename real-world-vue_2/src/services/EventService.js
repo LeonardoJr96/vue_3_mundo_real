7bb21fa854a00c196cb3aa1de4d06d0dd7e0ccfc
LeonardoJr96 <leo70x7000@gmail.com>
2024-07-26 11:15:28 -0300

    update commit: introduce database in web site with api

---
+++ real-world-vue_2/src/services/EventService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/leonardojr96/vue_3_mundo_real',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getEvents() {
+        return apiClient.get('/events')
+    }
+}
